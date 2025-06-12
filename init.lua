7b9fd672ecaead130bbbe3cceb60efe36b70b195
Gui <paulogui702@gmail.com>
2025-06-12 10:08:51 -0300

    ui config

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
--- The core of project
+-- The core
 -- import options: to put lines, numbers, tabs... You can see in lua/config/options
 require("config.options")
 
 -- import keymaps: to put shortcuts in keyboard to access features
 require("config.keymaps")
+
+-- import ui
+require("config.ui")
---
+++ lua/config/ui.lua
@@ -0,0 +1,18 @@
+-- NEOVIM ui configuration (Default theme)
+
+-- I-Beam cursor
+vim.cmd([[ set guicursor= ]])
+
+-- Default color pallet
+vim.cmd('colorscheme default')
+
+-- Transparent background
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight VertSplit guibg=NONE ctermbg=NONE
+  highlight StatusLine guibg=NONE ctermbg=NONE
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+]]
